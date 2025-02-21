917eb8b3f38e60780b6c60f068a081f1ed303d78
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2025-02-21 15:59:21 -0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::u8;
+
 fn main() {
     println!("Hello, world!");
+    iterate();
+}
+
+fn iterate() {
+    let mut i: u8 = 1;
+    while i < 9 {
+        println!("hello");
+        i = i + 1;
+    }
 }
